4a873ced0a5af92f7aed96e2749cf905bc2aa681
Nass213dz <nassouben44@gmail.com>
2024-12-06 07:11:13 +0100

    nhdgfbfs

--- script.js
+++ script.js
@@ -509,25 +509,25 @@ function dessiner() {
 
     ctx.drawImage(joueurImage, joueur.x, joueur.y, joueur.width, joueur.height);
 
-ctx.fillStyle = "orange";
-for (let missile of missiles) {
-    ctx.drawImage(missileImage, missile.x, missile.y, missile.width, missile.height);
-}
-
-if (!iaDetruite) {
-    ctx.drawImage(IAImage, ia.x, ia.y, ia.width, ia.height);
-}
-
-    ctx.fillStyle = "yellow";
-    ctx.font = "20px Arial";
-    ctx.fillText(`Score: ${score}`, 10, 20);
-    ctx.fillText(`Vies: ${vies}`, 10, 40);
-
-    if (gameOver) {
-        ctx.fillStyle = "red";
-        ctx.font = "40px Arial";
-        ctx.fillText("Game Over", canvas.width / 2 - 100, canvas.height / 2);
+    ctx.fillStyle = "orange";
+    for (let missile of missiles) {
+        ctx.drawImage(missileImage, missile.x, missile.y, missile.width, missile.height);
     }
+
+    if (!iaDetruite) {
+        ctx.drawImage(IAImage, ia.x, ia.y, ia.width, ia.height);
+    }
+
+        ctx.fillStyle = "yellow";
+        ctx.font = "20px Arial";
+        ctx.fillText(`Score: ${score}`, 10, 20);
+        ctx.fillText(`Vies: ${vies}`, 10, 40);
+
+        if (gameOver) {
+            ctx.fillStyle = "red";
+            ctx.font = "40px Arial";
+            ctx.fillText("Game Over", canvas.width / 2 - 100, canvas.height / 2);
+        }
 }
 
 function gameLoop() {
